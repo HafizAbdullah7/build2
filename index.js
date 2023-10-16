fb657c2743714de8b7e495ddcf3710a8eb171475
HafizAbdullah7 <hafizab200@gmail.com>
2023-10-16 05:44:45 -0700

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-
+const path = require('path');
 app.use(cors());
 
 app.get('/download', (req, res) => {
@@ -10,10 +10,8 @@ app.get('/download', (req, res) => {
   ytdl(url, { filter: format => format.container === 'mp4' })
     .pipe(res);
 });
+app.use(express.static(path.join(__dirname, "./build")));
 
-app.get('/', (req, res) => {
-  res.send('Hello, this is the YouTube Video Downloader server!');
-});
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
 });
